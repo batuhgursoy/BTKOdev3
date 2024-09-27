5d283789f47b18f2de586bff2edbf008b5088abe
Batuhan Gürsoy <batuhangursoy@Mac.home>
2024-09-27 17:08:54 +0300

    BTK 3.Odev

---
+++ BTKOdev3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BTKOdev3
+//
+//  Created by Batuhan Gürsoy on 27.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BTKOdev3UITests/BTKOdev3UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BTKOdev3UITestsLaunchTests.swift
+//  BTKOdev3UITests
+//
+//  Created by Batuhan Gürsoy on 27.09.2024.
+//
+
+import XCTest
+
+final class BTKOdev3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
